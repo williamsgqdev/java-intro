aa31a4a0935589cc636d9f973bee527cf153ac6b
Williams <88431152+williamsgqdev@users.noreply.github.com>
2023-07-01 15:01:11 +0100

    arit in java

---
+++ AritmeticExpression.java
@@ -0,0 +1,14 @@
+public class AritmeticExpression {
+    public static void main(String[] args) {
+
+        double friends = 10;
+        // friends = friends + 1;
+
+        // friends = friends % 2; // modulus : gets remainder
+        friends = (double) friends / 3; // cast to double
+
+        friends++;
+
+        System.out.println("Friends list is: " + friends);
+    }
+}
