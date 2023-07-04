41124de7abcd8da5b52e65d103961ad220c01c3c
Williams <88431152+williamsgqdev@users.noreply.github.com>
2023-07-04 14:29:36 +0100

    equals

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class LogicalOperators {
     public static void main(String[] args) {
-        int temp = 25;
+        // int temp = 25;
 
-        if (temp > 30) {
-            System.out.println("Its hot outside");
-        } else if (temp >= 20 && temp <= 30) {
-            System.out.println("Mild weather outside");
+        // if (temp > 30) {
+        // System.out.println("Its hot outside");
+        // } else if (temp >= 20 && temp <= 30) {
+        // System.out.println("Mild weather outside");
+        // } else {
+        // System.out.println("Its cold outside");
+        // }
+
+        /*
+         * 
+         * =============================================================================
+         * 
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enetr q or Q to quit game ");
+
+        String resp = scanner.next();
+
+        if (resp.equals("q") || resp.equals("Q")) {
+            System.out.println("You have successfully quit the game");
         } else {
-            System.out.println("Its cold outside");
+            System.out.println("You have failed to quit the game");
         }
 
     }
