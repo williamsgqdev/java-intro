9e20fcd89f6ef7265e2ca245013305b2e38b33de
Williams <88431152+williamsgqdev@users.noreply.github.com>
2023-07-09 11:20:53 +0100

    obj passing

---
+++ Garage.java
@@ -0,0 +1,5 @@
+public class Garage {
+    void park(Car car) {
+        System.out.println("You parked a " + car.make);
+    }
+}
---
+++ ObjectPassing.java
@@ -0,0 +1,9 @@
+public class ObjectPassing {
+    public static void main(String[] args) {
+        Garage garage = new Garage();
+
+        Car car = new Car();
+
+        garage.park(car);
+    }
+}
